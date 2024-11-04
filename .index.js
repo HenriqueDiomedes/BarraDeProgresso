bc02afe67b483da6aa7cb735fce4c20325da474f
Henrique Diomedes <j.hyyk77@gmail.com>
2024-11-04 09:47:33 -0300

    upfinal

--- .index.js
+++ .index.js
@@ -2,14 +2,14 @@ let valorBarra = document.querySelector('.valor_barra');
 let barra = document.querySelector('.barra');
 
 let valorAtualBarra = 0;
-let valorFinalBarra = 99;
+let valorFinalBarra = 60;   /*até onde a barra vai preencher*/
 
 let progresso = setInterval(()=>{
     valorAtualBarra++;
     valorBarra.textContent = `${valorAtualBarra}%`;
-    barra.style.background = `conic-gradient(rgb(78, 236, 20) %{valorAtualBarra * 3.6} deg, rgb(230, 244, 244) 0deg)`
+    barra.style.background = `conic-gradient(rgb(78, 236, 20) ${valorAtualBarra * 3.6}deg, rgb(230, 244, 244) 0deg)`;
     if (valorAtualBarra == valorFinalBarra){
         clearInterval(progresso);
     };
 
-}, 99);
+}, 1000);   /*velocidade de ppreenchimento da barra*/
