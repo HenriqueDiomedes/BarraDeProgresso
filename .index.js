ce1c7df5ec229124b83a143131f857794aed970c
Henrique Diomedes <j.hyyk77@gmail.com>
2024-11-04 08:38:41 -0300

    up

--- .index.js
+++ .index.js
@@ -7,8 +7,9 @@ let valorFinalBarra = 99;
 let progresso = setInterval(()=>{
     valorAtualBarra++;
     valorBarra.textContent = `${valorAtualBarra}%`;
-    barra.style.background = `conic-gradient(rgb(78, 236, 20) %{valorAtualBarra * 3.6}deg, rgb(230, 244, 244) 0deg)`
+    barra.style.background = `conic-gradient(rgb(78, 236, 20) %{valorAtualBarra * 3.6} deg, rgb(230, 244, 244) 0deg)`
     if (valorAtualBarra == valorFinalBarra){
         clearInterval(progresso);
     };
+
 }, 99);
